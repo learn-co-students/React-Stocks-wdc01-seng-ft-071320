92788786d02e93bd99505154d5f90cf622ffba9c
github username <github email address>
2020-09-21 15:31:21 -0400

    Done.

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,21 +1,25 @@
 import React from 'react'
 
-const Stock = () => (
-  <div>
+const Stock = (props) => {
+const {name, ticker, price, id} = props.stock
 
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">{
-            //Company Name
-          }</h5>
-        <p className="card-text">{
-            //ticker: stock price
-          }</p>
-      </div>
+return (<div>
+
+  <div className="card" onClick={() => props.handleStock(id)}>
+    <div className="card-body">
+      <h5 className="card-title">{
+          name
+        }</h5>
+      <p className="card-text">{
+         `${ticker}: ${price}`
+        }</p>
     </div>
-
-
   </div>
+
+
+</div>
 );
+
+}
 
 export default Stock
--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -8,7 +8,7 @@ class StockContainer extends Component {
       <div>
         <h2>Stocks</h2>
         {
-          //render the list of stocks here
+         this.props.stocks.map(stock => <Stock stock={stock} key={stock.id} handleStock={this.props.addStock}/>)
         }
       </div>
     );
@@ -16,4 +16,5 @@ class StockContainer extends Component {
 
 }
 
-export default StockContainer;
+export default StockContainer; 
+ 
