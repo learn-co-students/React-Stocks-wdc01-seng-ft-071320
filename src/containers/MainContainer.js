156353002844b54994d86de4a6ca20665c089132
JinaStrickland <Jina30@gmail.com>
2020-09-22 22:33:23 -0400

    finished

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -3,22 +3,110 @@ import StockContainer from './StockContainer'
 import PortfolioContainer from './PortfolioContainer'
 import SearchBar from '../components/SearchBar'
 
+const url = "http://localhost:3000/stocks"
+
 class MainContainer extends Component {
 
+  state={
+    stocks: [],
+    portfolioStocks: [],
+    sort: "All",
+    filter: "All"
+  }
+
+  componentDidMount() {
+    fetch(url)
+    .then(res => res.json())
+    .then(stocks => this.setState({ stocks: stocks }))
+  }
+
+  stockToPortfolio = (id) => {
+    if( !this.state.portfolioStocks.includes(id) ) {
+        this.setState({
+          portfolioStocks: [...this.state.portfolioStocks, id]
+        })
+    }
+  }
+
+  stockToRemove = (id) => {
+      this.setState({
+        portfolioStocks: this.state.portfolioStocks.filter( stockId => stockId !== id )
+      })
+  }
+
+  sortStatus = (status) => {
+    this.setState({
+      sort: status 
+    })
+  }
+
+  filterStatus = (status) => {
+    this.setState({
+      filter: status 
+    })
+  }
+
+  displaySortedStocks = () => {
+    let displayStocks = [...this.state.stocks]
+    
+    if(this.state.filter !== "All") {
+      displayStocks = displayStocks.filter(stock => stock.type === this.state.filter)
+    }
+
+    switch(this.state.sort) {
+      case "Alphabetically":
+        return displayStocks.sort((a, b) => a.name > b.name ? 1 : -1)
+      case "Price":
+        return displayStocks.sort((a, b) => a.price > b.price ? 1 : -1)
+      default:
+        return displayStocks
+    }
+
+    // if( this.state.sort === "Price") {
+    //   return displayStocks.sort((a, b) => a.price > b.price ? 1: -1)
+    // } 
+    //   else if ( this.state.sort === "Alphabetically") {
+    //   displayStocks.sort((a, b) => a.name > b.name ?  1: -1)
+    // } 
+    //   return this.state.stock 
+  }
+
+  // filterStock = (e) => {
+  //   let filteredStocks = [...this.state.stocks]
+  //     return filteredStocks.filter( stock => stock.type === e )
+  // }
+
+
   render() {
+
+    const myStocks = this.state.stocks.filter( 
+        stock => this.state.portfolioStocks.includes(stock.id))
+    
+    let displayTheStocks = this.displaySortedStocks()
+
     return (
       <div>
-        <SearchBar/>
+        <SearchBar
+          sortState={ this.state.sort }
+          filterState={ this.state.filter }
+          sortStatus={ this.sortStatus }
+          filterStatus={ this.filterStatus }
+          />
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer 
+                displayTheStocks={ displayTheStocks } 
+                stockToPortfolio={ this.stockToPortfolio}
+                />
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer 
+                myStocks={ myStocks } 
+                stockToRemove={ this.stockToRemove} />
 
             </div>
           </div>
